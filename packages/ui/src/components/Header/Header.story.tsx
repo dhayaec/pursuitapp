9a243cff0de4dddfd23931f070269f7b5f129048
Dhayanandhan.R <dhaya.r@partner.samsung.com>
2019-02-19 19:04:09 +0530

    remove unused

--- packages/ui/src/components/Header/Header.story.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import { Header } from './';
-
-storiesOf('Header', module).add('default', () => <Header>Hello World</Header>);
