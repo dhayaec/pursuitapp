5f2b74ff273348031248e5996c6598de8856295d
Dhayanandhan.R <dhaya.r@partner.samsung.com>
2019-03-14 11:09:50 +0530

    upgrade version

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,4 +1,5 @@
-import App, { Container } from 'next/app';
+import { ApolloClient, NormalizedCacheObject } from 'apollo-boost';
+import App, { Container, NextAppContext } from 'next/app';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { Provider } from 'reakit';
@@ -6,7 +7,11 @@ import theme from 'reakit-theme-default';
 import { GlobalStyle } from '../components/GlobalStyleSheet';
 import withApollo from '../lib/withApollo';
 
-class PursuitApp extends App<any> {
+interface NextAppContextExtended extends NextAppContext {
+  apolloClient: ApolloClient<NormalizedCacheObject>;
+}
+
+class PursuitApp extends App<NextAppContextExtended> {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
