d4a0b729b712d081f959167f6e32e33bad53fc4e
Dhayanandhan.R <dhaya.r@partner.samsung.com>
2019-07-25 11:20:10 +0530

    seperate vendor

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -17,7 +17,29 @@ const nextConfig = {
       reportFilename: './bundles/client.html',
     },
   },
-  webpack(config) {
+  webpack(config, options) {
+    // to avoid duplicating deps on every chunk
+    const { isServer, dev } = options;
+    if (!isServer && !dev) {
+      const cacheGroups = config.optimization.splitChunks.cacheGroups;
+
+      delete cacheGroups.react;
+
+      cacheGroups.default = false;
+
+      cacheGroups.vendors = {
+        name: 'vendors',
+        test: /[\\/](node_modules|packages)[\\/]/,
+        enforce: true,
+        priority: 20,
+      };
+
+      cacheGroups.commons = {
+        name: 'commons',
+        minChunks: 2,
+        priority: 10,
+      };
+    }
     return config;
   },
 };
